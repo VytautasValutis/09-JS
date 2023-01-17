18e04bc1049db72d4273a0e859e0f02f823fd0d3
Vytautas Valutis <dnc908@gmail.com>
2023-01-17 11:36:03 +0200

    work

---
+++ js/06-if.js
@@ -0,0 +1,20 @@
+console.clear();
+/*
+    if () {}
+    if () {} else {}
+    if () {} else if () {} 
+    if () {} else if () {} else {}
+    if () {} else if () {} ... else if () {}
+    if () {} else if () {} ... else {}
+
+Palyginimo operatoriai:
+    - visi: >, <, ==, !=, >=, <=, ===/!== (grieztai lygu/nelygu)
+    - naudotini: >, <, >=, <=, ===/!== (grieztai lygu/nelygu)
+    - nenaudotini: ==, !=
+*/
+if (4 > 2) {
+    console.log(`4>2`);
+}
+/*
+CODE NESTIG - nestinimas 
+*/
