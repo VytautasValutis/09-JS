869e2918e6c38f496768392fce33c0824fb1280c
Vytautas Valutis <dnc908@gmail.com>
2023-01-18 11:36:39 +0200

    Typeof isArray

---
+++ js/10-typeof.js
@@ -0,0 +1,23 @@
+console.clear();
+console.log(typeof 5);
+console.log(typeof Infinity);
+console.log(typeof NaN);
+console.log(typeof ``);
+console.log(typeof true);
+console.log(typeof function() {});
+console.log(typeof undefined);
+console.log(typeof NaN);
+console.log(typeof typeof 5);
+console.log(typeof []);   // !!!!!
+console.log(typeof null); // !!!!!!
+console.log(typeof [true]);
+console.log(typeof {});
+
+console.log(Array.isArray(5));
+console.log(Array.isArray(true));
+console.log(Array.isArray(``));
+console.log(Array.isArray([]));
+console.log(Array.isArray({}));
+
+
+
