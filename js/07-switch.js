34ef8374578b9e104f5a381283399498ba853625
Vytautas Valutis <dnc908@gmail.com>
2023-01-18 09:28:10 +0200

    Switch

---
+++ js/07-switch.js
@@ -0,0 +1,33 @@
+console.clear();
+const diena = 1;
+switch (diena) {
+    case 1:
+        console.log(`pirmadienis`);
+        break;
+    case 2:
+        console.log(`antradienis`);
+        break;
+    case 3:
+        console.log(`treciadienis`);
+        break;
+        
+    default:
+        console.log(`Tokios dienos nera`);
+        break;
+}
+switch (diena) {
+    case 1:
+    case 2:
+    case 3:
+    case 4:
+    case 5:
+        console.log(`darbo diena`);
+        break;
+    case 6:
+    case 7:
+        console.log(`savaitgalis`);
+        break;    
+    default:
+        console.log(`Tokios dienos nera`);
+        break;
+}
