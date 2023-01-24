9cc831d0859cf4824633bb29976a372e1a4e9b11
Vytautas Valutis <dnc908@gmail.com>
2023-01-24 11:49:54 +0200

    Object

--- js/17-object.js
+++ js/17-object.js
@@ -27,8 +27,47 @@ const person1o = {
     age: 99,
     isMarried: true,
 }
+const person2o = {
+    name: `Maryte`,
+    age: 88,
+    isMarried: false,
+}
 
 console.log(person1o);
 console.log(`Sveiki, mano vardas yra ${person1o['name']} ir man yta ${person1o['age']} metu. `);
+
+const car = {
+    brand: `Volvo`,
+    model: `S40`,
+    color: `black`,
+    price: 10000,
+    currency: `LTL`,
+}
+const kasManeDomina = 'model';
+console.log(car[kasManeDomina]); 
+
+const carIntro = `Parduoti ${car.brand} ${car.model} (${car.color}) uz ${car.price} ${car.currency}.`
+console.log(carIntro);
+
+const students = [
+    {
+        name: `Petras`,
+        marks: [10, 8, 6, 6]
+    },
+    {
+        name: `Maryte`,
+        marks: [10, 10, 2, 3, 5, 7]
+    }
+];
+
+console.log(students[0][`name`]);
+console.log(students[0].name);
+
+console.log(students[0].marks[1]);
+console.log(students[0][`marks`][1]);
+
+
+
+
 
 
