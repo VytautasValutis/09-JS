052eb48ad8d5ccfde982bf79135fe56142755ffa
Vytautas Valutis <dnc908@gmail.com>
2023-01-24 10:48:21 +0200

    Object

---
+++ js/17-object.js
@@ -0,0 +1,34 @@
+console.clear();
+
+/*
+OBJECT - duomenu tipas {}
+{
+    key1: 
+    key2:
+    key3:
+}
+*/
+
+const empty = {};
+console.log(empty);
+
+const perso1Name = `Petras`;
+const perso1Age = 99;
+const perso1isMarried = true;
+
+const perso2Name = `Maryte`;
+const perso2Age = 88;
+const perso2isMarried = false;
+
+const person1 = [`Petras`, 99, true];
+
+const person1o = {
+    name: `Petras`,
+    age: 99,
+    isMarried: true,
+}
+
+console.log(person1o);
+console.log(`Sveiki, mano vardas yra ${person1o['name']} ir man yta ${person1o['age']} metu. `);
+
+
